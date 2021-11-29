35c3e470c1b27ee56f30e54f3512c70af4e3e6fc
Leggacys <bogatu.daniel21@yahoo.com>
2021-11-29 14:45:22 +0200

    Update ortal shader

---
+++ IMR/Assets/Scripts/Core/PortalBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalBehaviour : MonoBehaviour
+{
+    
+    public Material[] materials;
+    public Shader shadersStencil;
+    public Shader shadersLit;
+
+    
+    private void Start()
+    {
+        foreach (var material in materials)
+        {
+            material.SetInt("_StencilTest",(int)CompareFunction.Equal);
+        }
+    }
+
+    
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (other.name != "AR Camera")
+            return;
+
+        //Outside of the portal
+        if (transform.position.z < other.transform.position.z)
+        {
+            foreach (var material in materials)
+            {
+                material.SetInt("_StencilTest",(int)CompareFunction.Equal);
+            }
+        }
+        //Inside the portal
+        else
+        {
+            foreach (var material in materials)
+            {
+                material.SetInt("_StencilTest",(int)CompareFunction.NotEqual);
+            }
+        }
+    }
+}
