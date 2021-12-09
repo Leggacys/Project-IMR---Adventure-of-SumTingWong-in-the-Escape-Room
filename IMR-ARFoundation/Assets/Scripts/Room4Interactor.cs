3fe150844fdb7071b2fa1d67d6466c16d0c4befe
Andrei <boogyman1989@yahoo.com>
2021-12-10 00:16:30 +0200

    united4

---
+++ IMR-ARFoundation/Assets/Scripts/Room4Interactor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room4Interactor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.H)){
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.forward, out hit,100))
+            { 
+                LeverInteract lever1 = hit.transform.gameObject.GetComponent<LeverInteract>();
+                if(lever1)
+                {
+                    lever1.Interact();
+                }
+            }
+
+        }
+    }
+}
