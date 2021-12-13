30ac8afea264c643a9c71a4aac090d771ba74d88
Andrei <boogyman1989@yahoo.com>
2021-12-13 19:03:06 +0200

    Restartable room3

--- IMR-ARFoundation/Assets/Scripts/Room3/Room3Manager.cs
+++ IMR-ARFoundation/Assets/Scripts/Room3/Room3Manager.cs
@@ -17,16 +17,34 @@ public class Room3Manager : MonoBehaviour
     // State currentState= State.STATE_ENTERED;
     public float penaltyValue;
     public float secondCount;
+    float initialSecondCount = -1;
     public GameObject UIText;
     public GameObject endText;
     public List<GameObject> puzzlePieces;
-    public GameObject puzzleToSpawn,ball;
+    public GameObject puzzleToSpawn,ball,roomPrefab;
+    Vector3 BallTransform = new Vector3(0.69f,0.69f,0.69f);
 
 
     Text UIValue;
 
     private void OnEnable() {
          UIValue = UIText.GetComponent<Text>();
+         if(initialSecondCount != -1)
+            secondCount = initialSecondCount;
+        else
+            initialSecondCount = secondCount;
+
+        if (BallTransform != new Vector3(0.69f,0.69f,0.69f))
+            ball.transform.position = BallTransform;
+        else
+            BallTransform = ball.transform.position;
+        
+        puzzleToSpawn.SetActive(false);
+        ball.SetActive(false);
+        endObject.SetActive(false);
+        endText.SetActive(false);
+        foreach(GameObject obj in puzzlePieces)
+            obj.SetActive(true);
         StartCoroutine("UITimer");
         StartCoroutine("PuzzleSpawn");
        
@@ -40,6 +58,8 @@ public class Room3Manager : MonoBehaviour
             yield return new WaitForSeconds(1);
         }
         endText.SetActive(true);
+        yield return new WaitForSeconds(3);
+        endGame(0);
 
 
     }
@@ -67,6 +87,18 @@ public class Room3Manager : MonoBehaviour
 
     public void GhostCollided(){
         secondCount -= penaltyValue;
+    }
+
+    public GameObject endObject;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Ball")
+            endObject.SetActive(true);
+            endGame(1);
+    }
+
+    void endGame(int final){
+        roomPrefab.SetActive(false);
     }
 
 
---
+++ IMR-ARFoundation/Assets/Scripts/lightonoff.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightonoff : MonoBehaviour
+{
+
+    //public GameObject txtToDisplay;             //display the UI text
+    
+    private bool PlayerInZone;                  //check if the player is in trigger
+
+    public GameObject lightorobj;
+    
+    //public GameObject light1, light2, light3, light4, light5;
+    public List<GameObject> lightList;
+
+    public GameObject key;
+
+    public bool isLightTriggered;
+
+    private void Start()
+    {
+
+        PlayerInZone = false;                   //player not in zone       
+        //txtToDisplay.SetActive(false);
+        StartCoroutine("CheckNumberOfLights");
+    }
+    
+
+
+    public void Interact()
+    {
+        if (PlayerInZone)
+        {
+            isLightTriggered = true;
+            StartCoroutine("SwitchLight");
+            gameObject.GetComponent<AudioSource>().Play();
+            gameObject.GetComponent<Animator>().Play("switch");
+            Debug.Log("Andrei Boss");
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")     //if player in zone
+        {
+            //txtToDisplay.SetActive(true);
+            PlayerInZone = true;
+        }
+     }
+    
+
+    private void OnTriggerExit(Collider other)     //if player exit zone
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerInZone = false;
+            //txtToDisplay.SetActive(false);
+        }
+    }
+
+    IEnumerator SwitchLight()
+    {
+        lightorobj.SetActive(!lightorobj.activeSelf);
+        for (int i = 0; i < lightList.Count ; i++)
+        {
+            if (lightList[i] != null)
+                lightList[i].SetActive(!lightList[i].activeSelf);
+            else
+            {
+                lightList.RemoveAt(i);
+                i--;
+            }
+        }
+        yield return new WaitForSeconds(5);
+        lightorobj.SetActive(!lightorobj.activeSelf);
+        for (int i = 0; i < lightList.Count ; i++)
+        {
+            if (lightList[i] != null)
+                lightList[i].SetActive(!lightList[i].activeSelf);
+            else
+            {
+                lightList.RemoveAt(i);
+                i--;
+            }
+        }
+        isLightTriggered = false;
+    }
+
+    IEnumerator CheckNumberOfLights()
+    {
+        
+        while (true)
+        {
+            if (lightList.Count == 0)
+            {
+                Debug.Log("LA MULTI ANI ANDREI!");
+                key.SetActive(!key.activeSelf);
+                break;
+            }
+
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
+
+
